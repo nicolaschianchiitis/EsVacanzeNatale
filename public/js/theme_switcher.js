2725e0857ad1bda32b3e9b532c81b69b22aa5cef
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2026-01-04 19:04:42 +0100

    Rifatto CSS e index.html

--- public/js/theme_switcher.js
+++
@@ -1,4 +0,0 @@
-const isDark = window.matchMedia('(prefers-color-scheme:dark)').matches
-const html = document.getElementById('html')
-
-isDark ? html.setAttribute('data-bs-theme', 'dark') : html.setAttribute('data-bs-theme', 'light')
